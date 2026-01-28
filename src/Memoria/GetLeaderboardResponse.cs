cf69cb5542cb48359b4711fc78feb14cfe0ba911
庄子 <>
2026-01-28 14:33:41 +0900

    feat: GetLeaderboardResponseのプロパティをinitに変更

--- src/Memoria/GetLeaderboardResponse.cs
+++ src/Memoria/GetLeaderboardResponse.cs
@@ -5,7 +5,7 @@ namespace Memoria;
 /// <summary>
 /// リーダーボード取得レスポンス.
 /// </summary>
-public class GetLeaderboardResponse
+public record GetLeaderboardResponse
 {
     /// <summary>
     /// リーダーボード取得結果.
@@ -17,37 +17,37 @@ public class GetLeaderboardResponse
 /// <summary>
 /// リーダーボード取得結果.
 /// </summary>
-public class GetLeaderboardResult
+public record GetLeaderboardResult
 {
     /// <summary>
     /// リーダーボードエントリーの配列.
     /// </summary>
     [JsonPropertyName("Leaderboard")]
-    public PlayerLeaderboardEntry[] Leaderboard { get; set; } = null!;
+    public PlayerLeaderboardEntry[] Leaderboard { get; init; } = null!;
 }
 
 /// <summary>
 /// プレイヤーのリーダーボードエントリー.
 /// </summary>
-public class PlayerLeaderboardEntry
+public record PlayerLeaderboardEntry
 {
     /// <summary>
     /// 表示名.
     /// </summary>
-    public string DisplayName { get; set; } = string.Empty;
+    public string DisplayName { get; init; } = string.Empty;
 
     /// <summary>
     /// PlayFab ID.
     /// </summary>
-    public string PlayFabId { get; set; } = string.Empty;
+    public string PlayFabId { get; init; } = string.Empty;
 
     /// <summary>
     /// 順位.
     /// </summary>
-    public int Position { get; set; }
+    public int Position { get; init; }
 
     /// <summary>
     /// 統計情報の値.
     /// </summary>
-    public int StatValue { get; set; }
+    public int StatValue { get; init; }
 }
